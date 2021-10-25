be99ac0d1c20abf6b090d6d8325128a918089c93
ubaka2 <89397110+ubaka2@users.noreply.github.com>
2021-10-25 16:21:28 -0500

    commit

--- jsfiles/index.js
+++ jsfiles/index.js
@@ -9,7 +9,7 @@ window.onload = function () {
           minute--;
         //   sec = 60;
           if (minute <0) {
-             window.location.replace("/htmlfiles/Q_page_story_1.html")
+             window.location.replace("../htmlfiles/Q_page_story_1.html")
 
           }
        }
